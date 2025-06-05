ac01c872f9f5f44fafa406d0a0090af03d71db01
Maximus2012 <mkochetkov9@gmail.com>
2025-06-05 23:12:09 +0600

    Changed docker

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY", "default")
 
-DEBUG = bool(os.getenv("DEBUG", True))
+DEBUG = bool(os.getenv("DEBUG", False))
 
 HOSTS = os.getenv("ALLOWED_HOSTS", "")
 
