b2f23df92d90a6ae4be5fe0d97476125288b5301
Maximus2012 <mkochetkov9@gmail.com>
2025-06-05 18:00:44 +0600

    Normal link

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -5,4 +5,4 @@ from .models import Recipe
 
 def short_link_redirect(request, pk):
     recipe = get_object_or_404(Recipe, pk=pk)
-    return redirect(f'/recipes/{recipe.pk}/')
+    return redirect(f'/recipes/{recipe.pk}')
